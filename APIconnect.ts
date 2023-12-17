f71c83d82aafd1cd32d3ddb39455297bb12c8dfd
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2023-12-17 17:20:26 -0600

    post part

--- APIconnect.ts
+++ APIconnect.ts
@@ -3,21 +3,42 @@ import fetch, { Response } from 'cross-fetch';
 
 type APIToken = {"token_type": string, "expires_in": number, "access_token": string 
   }
+    
+type AccessToken= {
+        aud: string,
+        jti: string,
+        iat: number,
+        nbf: number,
+        exp: number,
+        sub: string,
+        scopes: []
+      
+  }
   
-  const clientId = '9ZeAJ8bq9iRpaN5vGoXr7WJYfEMUm0Ir5jsdfTZqrTKSFWuT1Q';
-  const clientSecret = 'EKCklXdaxkyEmCh6KWlpffC5KXWUhD7oim88Hwls';
-  const url = 'https://api.petfinder.com/v2/oauth2/token';
+  async function getPetfinderAccessToken(clientId: string, clientSecret: string): Promise<AccessToken> {
+    const url = 'https://api.petfinder.com/v2/oauth2/token';
   
-  const data = new URLSearchParams({
+    const data = new URLSearchParams({
       grant_type: 'client_credentials',
       client_id: clientId,
       client_secret: clientSecret,
-    })
-   fetch(url, {method: 'POST',body: data})
-      .then((response: Response) => response.json())
-      .then((data:APIToken) => {
-        const token = jwtDecode(data.access_token)
-        const expirationDate = new Date().getTime() + data.expires_in * 1000;
-         console.log(token, expirationDate);
-      });
+    });
+  
+
+    const response: Response = await fetch(url, {method: 'POST',body: data})
+
+    const data_f:APIToken = await response.json()
+    const token:AccessToken = jwtDecode(data_f.access_token)
     
+    return token
+  }
+
+  async function sendReq() {
+    const clientId = '9ZeAJ8bq9iRpaN5vGoXr7WJYfEMUm0Ir5jsdfTZqrTKSFWuT1Q';
+    const clientSecret = 'EKCklXdaxkyEmCh6KWlpffC5KXWUhD7oim88Hwls';
+  
+    const token = await getPetfinderAccessToken(clientId, clientSecret);
+   console.log(token)
+  }
+  
+ sendReq();
