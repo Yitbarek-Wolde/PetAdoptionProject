f3d3aaca470dc764da4c6964b11568fbf4831dfb
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2023-12-19 14:17:27 -0600

    almost done

--- petQuestions.ts
+++ petQuestions.ts
@@ -4,13 +4,13 @@ import { sendReq, AccessToken, APIToken } from "./APIconnect";
 import { LocalStorage } from "node-localstorage";
 import { Root, Animal, pet, base, BookType, BookTypeObject } from "./types";
 
-type st ={
-    typed: string | undefined
+type st = {
+    typed: string
     named: string
-   breed: string
-    size:  string
+    breed: string
+    size: string
     age: string
-    color : string | undefined
+    color: string | undefined
     Status: string
 
 }
@@ -46,7 +46,7 @@ async function getPets() {
     const quaryParameter = Object.entries(userInput)
     let withName = "?" + quaryParameter[0][0] + "=" + quaryParameter[0][1] +
         "&" + quaryParameter[1][0] + "=" + quaryParameter[1][1] + "&" + quaryParameter[2][0] + "=" + quaryParameter[2][1]
-    
+
     let noName = "?" + quaryParameter[1][0] + "=" + quaryParameter[1][1] + "&" + quaryParameter[2][0] + "=" + quaryParameter[2][1]
 
     if (!quaryParameter[0][1])
@@ -60,22 +60,21 @@ async function fetchAnimals() {
     try {
         let time = Number(localStorage.getItem('exp'))
         let now = Math.floor(Date.now() / 1000)
-        let getInput = await getPets()
         if (time < now) {
             sendReq()
         }
-
+        let getInput = await getPets()
         let grant = localStorage.getItem('fullToken')
         let a = `https://api.petfinder.com/v2/animals${getInput}`
         const head = {
             Authorization: `Bearer ${grant}`
         };
 
-        const response:Response = await fetch(a, { method: 'GET', headers: head })
+        const response: Response = await fetch(a, { method: 'GET', headers: head })
         const data: Root = await response.json()
         let collect = data.animals.map((a: Animal) => ({ title: a.name, value: a.id }))
         localStorage.setItem('bookmark', JSON.stringify(collect))
-        
+
     }
     catch (error) {
         console.log(error)
@@ -84,18 +83,18 @@ async function fetchAnimals() {
 
 async function getPetsById() {
     await fetchAnimals()
-    let choice:BookType = JSON.parse(localStorage.getItem("bookmark") || "[]") 
-    const userInput = await prompts([
+    let choice: BookType = JSON.parse(localStorage.getItem("bookmark") || "[]")
+    const userInput = await prompts(
         {
             type: "select",
             name: "type",
             message: "select animal type",
             choices: choice
-        }])
-    const quaryParameter:BookType[] = Object.entries(userInput)
-    let quary = quaryParameter[0][1]
-
-    return quary.value
+        })
+    const quaryParameter = Object.entries(userInput)
+    let quary:number = quaryParameter[0][1]
+        
+    return quary
 }
 
 async function fetchAnimalsById() {
@@ -108,40 +107,40 @@ async function fetchAnimalsById() {
     };
  const response:Response = await fetch(a, { method: 'GET', headers: head })
     const data:base = await response.json()
+    localStorage.setItem('fullRes', JSON.stringify(data))
     let store: st[] = [];
     store.push({
-      typed: data.animal.type,
+      typed: data.animal.name,
       named: data.animal.name,
       breed: data.animal.breeds.primary,
       size: data.animal.size,
       age: data.animal.age,
       color: data.animal.colors.primary,
       Status: data.animal.status,
-    });
+    })
 
     localStorage.setItem('SelectedAnimal', JSON.stringify(store))
 }
 
-async function displayAnimal(){
-   await fetchAnimalsById() 
-   let dataf:st[] = JSON.parse(localStorage.getItem("SelectedAnimal") || '[]')
-    dataf.forEach(data=>console.log(`
-Here is your ${data.typed} details
-    Name : ${data.named} 
-    Breed : ${data.breed }
-    Size : ${data.size }
-    Age : ${data.age}
-    Color: ${data.color }
-    Status : ${data.Status}`))
-   
+async function displayAnimal() {
+    await fetchAnimalsById()
+    let dataf: st[] = JSON.parse(localStorage.getItem("SelectedAnimal") || '[]')
+    dataf.forEach(data => console.log(`
+    Here is your ${data.typed} details
+        Name : ${data.named} 
+        Breed : ${data.breed}
+        Size : ${data.size}
+        Age : ${data.age}
+        Color: ${data.color}
+        Status : ${data.Status}`))
 }
 
-function displayBookmark(){
-    let saved:BookType = JSON.parse(localStorage.getItem("bookmark") || "[]") 
-    if(!saved)
+function displayBookmark() {
+    let saved: BookType = JSON.parse(localStorage.getItem("bookmark") || "[]")
+    if (!saved)
         console.log('Bookmark empty')
     else
-        saved.forEach((s:BookTypeObject)=> console.log(s))
+        saved.forEach((s: BookTypeObject) => console.log(s))
 }
 
 async function Menu() {
@@ -152,16 +151,19 @@ async function Menu() {
             message: "Enter 1 to find new Pet or 2 to see bookmark list or 3 to Exit ",
             validate: (value: string) => (value !== "1" && value !== "2" && value !== "3") ? "Please enter either 1 or 2" : true,
         }
-    
+
     ])
-    if (userInput.name === "1") {
-       await displayAnimal()
-      } else if (userInput.name === "2") {
+    if (userInput.name === "1") 
+        await displayAnimal()
+    
+    else if (userInput.name === "2") 
         displayBookmark()
-      } else if (userInput.name === "3") {
+
+     else {
         console.log("Exiting...");
         return
-      }
-    Menu()
     }
+   
+    Menu()
+}
 Menu()
